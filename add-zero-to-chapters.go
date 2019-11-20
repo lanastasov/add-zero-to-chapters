8a6b1079460a1cb2df7fa35f0c851e2e0c0f7321
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2019-11-20 16:10:55 +0200

    adding zeros lesson1.mp4 -> lesson 001.mp4

--- add-zero-to-chapters.go
+++ add-zero-to-chapters.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+	"regexp"
 )
 
 func main() {
+	fmt.Println("--add-zero-to-chapters.go--")
+	
+	re := regexp.MustCompile(`\D\d{1}\.`)
+  	re2:= regexp.MustCompile(`\D\d{2}\.`)
+	
 	files, _ := filepath.Glob("*.*")
 	for i := range files {
 		if (files[i][0] == 49 && files[i][1] == 46) ||
@@ -19,6 +27,22 @@ func main() {
 			(files[i][0] == 57 && files[i][1] == 46) {
 			_ = os.Rename(files[i], "0"+files[i])
 		}
+
+		v := re.FindString(files[i])
+		if v != "" {
+			v = strings.TrimLeft(v, string(v[0]))
+			v = strings.TrimRight(v, string(v[len(v)-1]))
+			_ = os.Rename(files[i], strings.Replace(files[i], v, " 00"+v, 1))
+		}
+		
+
+		v = re2.FindString(files[i])
+		if v != "" {
+			v = strings.TrimLeft(v, string(v[0]))
+     		v = strings.TrimRight(v, string(v[len(v)-1]))
+			_ = os.Rename(files[i], strings.Replace(files[i], v, " 0"+v, 1))
+		}
+		
 	}
 
 	files, _ = filepath.Glob("*.*")
