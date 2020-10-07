10686a5175acf4a874599cbfabcc410ee56ccbeb
lanastasov <lubomir.anastasov@gmail.com>
2020-10-07 23:00:52 +0300

    add support for - and the regex should be changed

--- add-zero-to-chapters.go
+++ add-zero-to-chapters.go
@@ -16,15 +16,15 @@ func main() {
 
 	files, _ := filepath.Glob("*.*")
 	for i := range files {
-		if (files[i][0] == 49 && files[i][1] == 46) ||
-			(files[i][0] == 50 && files[i][1] == 46) ||
-			(files[i][0] == 51 && files[i][1] == 46) ||
-			(files[i][0] == 52 && files[i][1] == 46) ||
-			(files[i][0] == 53 && files[i][1] == 46) ||
-			(files[i][0] == 54 && files[i][1] == 46) ||
-			(files[i][0] == 55 && files[i][1] == 46) ||
-			(files[i][0] == 56 && files[i][1] == 46) ||
-			(files[i][0] == 57 && files[i][1] == 46) {
+		if (files[i][0] == 49 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 50 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 51 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 52 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 53 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 54 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 55 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 56 && (files[i][1] == 46 || files[i][1] == 45)) ||
+			(files[i][0] == 57 && (files[i][1] == 46 || files[i][1] == 45)) {
 			_ = os.Rename(files[i], "0"+files[i])
 		}
 
